2f11f877614559e23083c661c345b759fbdacbe5
Seth Seeman <sseeman@pdx.edu>
2020-05-23 16:10:39 -0700

    import script testing

---
+++ connection_test.py
@@ -0,0 +1,16 @@
+# CS588 Final project
+# Seth Seeman
+# Spring 2020
+#
+# test db connection script
+
+import pymongo, sys
+
+ip = sys.argv[1]
+
+usr = "root"
+psswrd = "super!secret"
+
+client = pymongo.MongoClient(
+        "mongodb://" + usr + ":" + psswrd + "@" + ip + "/test?retryWrites=true&w=majority")
+db = client.freeway 
--- import_freeway.py
+++ import_freeway.py
@@ -2,14 +2,56 @@
 # Seth Seeman
 # Spring 2020
 #
-#
+# script to import all freeway data csv files to new mongoDB database
 
-import pymongo, sys
+import pymongo, sys, csv
 
-ip = sys.argv[1]
+try:
+        ip = "35.235.127.249"
+        #ip = sys.argv[1]
+except:
+        print("Must provide DB IP-Address as first argument")
+        exit(1)
+try:
+        data_dir = "sample_data/"
+        #data_dir = sys.argv[2]
+except:
+        print("Must provide data directory as second argument")
+        exit(1)
+
 usr = "root"
 psswrd = "super!secret"
 
-client = pymongo.MongoClient(
-        "mongodb://" + usr + ":" + psswrd + "@" + ip + "/test?retryWrites=true&w=majority")
-db = client.freeway 
+client = pymongo.MongoClient("mongodb://" + usr + ":" + psswrd + "@" + ip)
+db = client["freeway"] 
+db.list_collection_names()
+'''
+detector_file = data_dir + "/detectors.csv"
+station_file = data_dir + "/stations.csv"
+highway_file = data_dir + "/highways.csv"
+loopdata_file = data_dir + "/freeway_loopdata.csv"
+
+detectors = db["detectors"]
+
+with open(detector_file, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(csv_reader)       # skips first line (column headers)
+        for line in csv_reader:
+                #detectorid = line[0]
+                #highwayid = line[1]
+                #milepost = line[2]
+                #locationtext = line[3]
+                #detectorclass = line[4]
+                #lanenumber = line[5]
+                #stationid = line[6]
+                
+                detector_dict = { "detectorid" : line[0],
+                                "highwayid" : line[1],
+                                "milepost" : line[2],
+                                "locationtext" : line[3],
+                                "detectorclass" : line[4],
+                                "lanenumber" : line[5],
+                                "stationid" : line[6]
+                }
+                result = detectors.insert_one(detector_dict)
+                print(result)'''
