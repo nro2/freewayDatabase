0d7b2fb5f3bac09fcc56d3a6f798072c48e5c3a6
Seth Seeman <sseeman@pdx.edu>
2020-05-26 14:51:48 -0700

    queries 1 and 2 added to test file

--- test_queries.py
+++ test_queries.py
@@ -14,14 +14,32 @@ psswrd = "super!secret"
 
 client = pymongo.MongoClient("mongodb://" + usr + ":" + psswrd + "@" + ip + ":27017/")
 db = client.freeway 
-print ("\n#---------------------------------------------#")
-# query 1
-cols = db.list_collection_names()
-print(cols)
 print ("#---------------------------------------------#")
-# query 2 - all detector ids less than 1350
-query2 = {"detectorid": { "$lt": "1350" }}
-detectors = db.detectors.find(query2)
-for detector in detectors:
-        pprint(detector)
+# QUERY 1 - Count high speeds: Find the number of speeds > 100 in the data set.
+#
+# 
+reading_count = db.loopdata.find({"speed": {"$gt": 100}}).count()
+print("Count of 100+ speeds: " + str(reading_count))
+
+print ("#---------------------------------------------#")
+# QUERY 2 - Volume: Find the total volume for the station Foster NB for Sept 21, 2011.
+#
+# find station id for Foster NB
+query = {"locationtext": "Foster NB"}
+detectors = db.detectors.find_one(query)
+station = detectors['stationid']
+# query all loopdata for station id and for sept 21, 2011 - return volume 
+readings = db.loopdata.find({"stationid": '1045', "date": '2011-09-15'},{"volume" : 1})
+total = 0
+for reading in readings:
+    if reading['volume'] != '':
+        # sum volumes
+        total += int(reading['volume'])
+print("Volume: " + str(total))
+
+
+
 print ("#---------------------------------------------#\n")
+#readings = db.loopdata.find()
+#for reading in readings:
+    #print(reading)
