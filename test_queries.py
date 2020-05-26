c2173065b920681c896c1d3d8eeeeb51db1b4cac
Seth Seeman <sseeman@pdx.edu>
2020-05-26 15:47:32 -0700

    test query file organization and pretty printing

--- test_queries.py
+++ test_queries.py
@@ -14,16 +14,22 @@ psswrd = "super!secret"
 
 client = pymongo.MongoClient("mongodb://" + usr + ":" + psswrd + "@" + ip + ":27017/")
 db = client.freeway 
+print ("\n###############################################")
+#print ("#---------------------------------------------#\n")
+# print all loopdata - for testing/verification
+#readings = db.loopdata.find()
+#for reading in readings:
+    #print(reading)
+    
 print ("#---------------------------------------------#")
 # QUERY 1 - Count high speeds: Find the number of speeds > 100 in the data set.
-#
-# 
+print("Query 1")
 reading_count = db.loopdata.find({"speed": {"$gt": 100}}).count()
 print("Count of 100+ speeds: " + str(reading_count))
 
 print ("#---------------------------------------------#")
 # QUERY 2 - Volume: Find the total volume for the station Foster NB for Sept 21, 2011.
-#
+print("Query 2")
 # find station id for Foster NB
 query = {"locationtext": "Foster NB"}
 detectors = db.detectors.find_one(query)
@@ -37,9 +43,25 @@ for reading in readings:
         total += int(reading['volume'])
 print("Volume: " + str(total))
 
+print ("#---------------------------------------------#")
+# QUERY 3 - Single-Day Station Travel Times: Find travel time for station Foster NB for 5-minute intervals for Sept 22, 2011. 
+# Report travel time in seconds.
+print("Query 3")
+
+print ("#---------------------------------------------#")
+# QUERY 4 - Peak Period Travel Times: Find the average travel time for 7-9AM and 4-6PM on September 22, 2011 for station Foster NB. 
+# Report travel time in seconds.
+print("Query 4")
+
+print ("#---------------------------------------------#")
+# QUERY 5 - Peak Period Travel Times: Find the average travel time for 7-9AM and 4-6PM on September 22, 2011 for the I-205 NB freeway. 
+# Report travel time in minutes.
+print("Query 5")
+
+print ("#---------------------------------------------#")
+# QUERY 6 - Route Finding: Find a route from Johnson Creek to Columbia Blvd on I-205 NB using the upstream and downstream fields.
+print("Query 6")
 
 
-print ("#---------------------------------------------#\n")
-#readings = db.loopdata.find()
-#for reading in readings:
-    #print(reading)
+print ("#---------------------------------------------#")
+print ("###############################################\n")
