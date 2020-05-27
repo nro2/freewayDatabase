085d02303d821f6b27ee85be1cda56c159e3e521
Scott <srubey.hotmail.com>
2020-05-27 20:07:42 +0000

    querry 3 working

--- test_queries.py
+++ test_queries.py
@@ -48,6 +48,57 @@ print ("#---------------------------------------------#")
 # Report travel time in seconds.
 print("Query 3")
 
+intvlList = []
+counter = 0
+totSpeeds = 0
+avgSpeed = 0
+intvlTime = 0
+lastIntvl = False
+
+#find stationid for "Foster NB"
+query = {"locationtext": "Foster NB"}
+detectors = db.detectors.find_one(query)
+stationNum = detectors['stationid']
+
+query2 = {"stationid": "1045"}  #***replace "1045" with 'stationNum'***
+station = db.stations.find_one(query2)
+length = station['length']
+
+#query all loopdata for station id and for sept 21, 2011
+readings = db.loopdata.find({"stationid": '1045', "date": '2011-09-15'})  #***replace '1045' with 'station' and '2011-09-22'***
+for reading in readings:
+    newIntvl = False
+    lastIntvl = False
+    timestamp = reading['time']
+    minutes = timestamp[3:5]
+    seconds = timestamp[6:8]
+
+    if(int(minutes) % 5 == 0) and seconds == '00':
+        newIntvl = True
+#        print("***newIntvl***")  #debug
+    elif(int(minutes) % 5 == 4) and seconds == '40':
+        lastIntvl = True
+#        print("***lastIntvl***")  #debug
+
+#    print("Minutes:", minutes)  #debug
+#    print("Seconds:", seconds)  #debug
+   
+    if newIntvl:
+        totSpeeds = 0
+        counter = 0
+        intvlTime = timestamp
+
+    if reading['speed'] != '':
+        totSpeeds += (int(reading['speed']) * int(reading['volume']))
+        counter += int(reading['volume'])
+#        print("TotSpeeds:", totSpeeds, "Counter:", counter)  #debug
+
+    if lastIntvl:
+        avgSpeed = totSpeeds / counter
+        time = (float(length) / avgSpeed) * 3600
+        intvlList.append(tuple((intvlTime, time)))
+
+
 print ("#---------------------------------------------#")
 # QUERY 4 - Peak Period Travel Times: Find the average travel time for 7-9AM and 4-6PM on September 22, 2011 for station Foster NB. 
 # Report travel time in seconds.
